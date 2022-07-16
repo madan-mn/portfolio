befcf2b11abe384bbf7492e7aa9609b2c235c4db
Madan MN <madan.mn@yahoo.com>
2022-07-16 23:44:30 +0530

    Updates

---
+++ precache-manifest.fc06f6a827bcedbebdb7d3c7c9ade98b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ee55a38fb7d57ccc005221a4943a12",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "059853c46683b058e228",
+    "url": "/portfolio/static/css/2.cd68d35a.chunk.css"
+  },
+  {
+    "revision": "287b8818fdd8f29b5f41",
+    "url": "/portfolio/static/css/main.87c3c522.chunk.css"
+  },
+  {
+    "revision": "059853c46683b058e228",
+    "url": "/portfolio/static/js/2.f40aadfe.chunk.js"
+  },
+  {
+    "revision": "98a47d2fbeaba92d2146e086bd705323",
+    "url": "/portfolio/static/js/2.f40aadfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287b8818fdd8f29b5f41",
+    "url": "/portfolio/static/js/main.7ec5a4be.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "fc2aff752488d4117622424fe67ac164",
+    "url": "/portfolio/static/media/Oswald-Bold.fc2aff75.ttf"
+  },
+  {
+    "revision": "fd13770a30eabe63961427fbc1af6ca0",
+    "url": "/portfolio/static/media/Oswald-ExtraLight.fd13770a.ttf"
+  },
+  {
+    "revision": "1ba429bafeb7e7497970c07ed701a77f",
+    "url": "/portfolio/static/media/Oswald-Light.1ba429ba.ttf"
+  },
+  {
+    "revision": "bee2e4e7d4a203c9647e8032aade5bf8",
+    "url": "/portfolio/static/media/Oswald-Medium.bee2e4e7.ttf"
+  },
+  {
+    "revision": "7ce2b67f59ccf4d6c74f9b723ff2d2ae",
+    "url": "/portfolio/static/media/Oswald-Regular.7ce2b67f.ttf"
+  },
+  {
+    "revision": "c816f692e49bcaa814c936d8d2ba8bf7",
+    "url": "/portfolio/static/media/Oswald-SemiBold.c816f692.ttf"
+  },
+  {
+    "revision": "b8e42971dec8d49207a8c8e2b919a6ac",
+    "url": "/portfolio/static/media/Roboto-Bold.b8e42971.ttf"
+  },
+  {
+    "revision": "881e150ab929e26d1f812c4342c15a7c",
+    "url": "/portfolio/static/media/Roboto-Light.881e150a.ttf"
+  },
+  {
+    "revision": "8a36205bd9b83e03af0591a004bc97f4",
+    "url": "/portfolio/static/media/Roboto-Regular.8a36205b.ttf"
+  },
+  {
+    "revision": "66209ae01f484e46679622dd607fcbc5",
+    "url": "/portfolio/static/media/Roboto-Thin.66209ae0.ttf"
+  },
+  {
+    "revision": "a4f82f973a393c8a7a59d404288cd2d4",
+    "url": "/portfolio/static/media/background.a4f82f97.jpg"
+  },
+  {
+    "revision": "1c347da818508502c30c429a4640721c",
+    "url": "/portfolio/static/media/profile.1c347da8.png"
+  }
+]);
